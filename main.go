aaa679e60c412d3e8f8a02b62f00416596c0cf1e
txn7712 <txn7712@yum.com>
2024-10-15 14:50:56 +0200

    import alias

--- main.go
+++ main.go
@@ -4,23 +4,23 @@ import (
 	"fmt"
 	"os"
 
-	"calculator/utils" //precede with package go.mod name
+	u "calculator/utils" //precede with package go.mod name
 )
 
 func main() {
 	var x, y float64
 	var o string
 
-	x = utils.CallFuncExitOnError(utils.RequestInput, "Enter a number \n", x)
-	y = utils.CallFuncExitOnError(utils.RequestInput, "Enter a number \n", x)
-	o = utils.CallFuncExitOnError(utils.RequestInput, "Enter operator +, -, * or / \n", o)
+	x = u.CallFuncExitOnError(u.RequestInput, "Enter a number \n", x)
+	y = u.CallFuncExitOnError(u.RequestInput, "Enter a number \n", x)
+	o = u.CallFuncExitOnError(u.RequestInput, "Enter operator +, -, * or / \n", o)
 
-	if utils.Operations[o] == nil {
+	if u.Operations[o] == nil {
 		fmt.Printf("operation %s is not supported", o)
 		os.Exit(1)
 	}
 
-	res, err := utils.Operations[o](x, y)
+	res, err := u.Operations[o](x, y)
 
 	if err != nil {
 		fmt.Println(err)
